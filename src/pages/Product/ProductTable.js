c763402e9c1489b31a6c27ffe790a2a82d724b81
sksabbirhossain <sksabbirhossain4@gmail.com>
2023-07-29 14:23:04 +0600

    showing all product info in product table

--- src/pages/Product/ProductTable.js
+++ src/pages/Product/ProductTable.js
@@ -2,7 +2,6 @@ import React from "react";
 import { FaPencilAlt, FaTrashAlt } from "react-icons/fa";
 
 export const ProductTable = ({ products }) => {
-  const status = false;
   return (
     <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
       <table className="w-full text-sm text-left text-gray-500">
@@ -29,40 +28,45 @@ export const ProductTable = ({ products }) => {
           </tr>
         </thead>
         <tbody>
-          <tr className="bg-white border-b">
-            <th
-              scope="row"
-              className="px-6 py-3 font-medium text-gray-900 whitespace-nowrap"
-            >
-              <img
-                src="https://static.gadgetandgear.com/tmp/product/20221013_1665649900_129385.jpeg"
-                alt="brand"
-                className="w-11 h-11 rounded-full ring-2 ring-green-700 p-1"
-              />
-            </th>
-            <td className="px-6 py-3 capitalize">iphone 14 pro max</td>
-            <td className="px-6 py-3 capitalize">$1199</td>
-            <td className="px-6 py-3 capitalize">10</td>
-            <td className="px-6 py-3 cursor-pointer">
-              {status ? (
-                <span className="text-white font-semibold px-3 py-1 rounded-md bg-green-700">
-                  Active
-                </span>
-              ) : (
-                <span className="text-white font-semibold px-3 py-1 rounded-md bg-red-600">
-                  Inactive
-                </span>
-              )}
-            </td>
-            <td className="px-6 py-3 sm:space-x-2 space-x-1">
-              <button className="text-[16px] text-white bg-green-700 p-1 rounded-md">
-                <FaPencilAlt />
-              </button>
-              <button className="text-[16px] text-white bg-red-700 p-1 rounded-md">
-                <FaTrashAlt />
-              </button>
-            </td>
-          </tr>
+          {products?.map((product) => {
+            const { name, picture, price, stock, status } = product;
+            return (
+              <tr className="bg-white border-b">
+                <th
+                  scope="row"
+                  className="px-6 py-3 font-medium text-gray-900 whitespace-nowrap"
+                >
+                  <img
+                    src={`${process.env.REACT_APP_BASE_URL}/uploads/${picture}`}
+                    alt="brand"
+                    className="w-11 h-11 rounded-full ring-2 ring-green-700 p-1"
+                  />
+                </th>
+                <td className="px-6 py-3 capitalize">{name}</td>
+                <td className="px-6 py-3 capitalize">${price}</td>
+                <td className="px-6 py-3 capitalize">{stock}</td>
+                <td className="px-6 py-3 cursor-pointer">
+                  {status ? (
+                    <span className="text-white font-semibold px-3 py-1 rounded-md bg-green-700">
+                      Active
+                    </span>
+                  ) : (
+                    <span className="text-white font-semibold px-3 py-1 rounded-md bg-red-600">
+                      Inactive
+                    </span>
+                  )}
+                </td>
+                <td className="px-6 py-3 sm:space-x-2 space-x-1">
+                  <button className="text-[16px] text-white bg-green-700 p-1 rounded-md">
+                    <FaPencilAlt />
+                  </button>
+                  <button className="text-[16px] text-white bg-red-700 p-1 rounded-md">
+                    <FaTrashAlt />
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
