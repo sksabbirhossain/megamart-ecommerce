8e4d6dde4979de051a3d98daa9768dbfbd7dde54
sksabbirhossain <sksabbirhossain4@gmail.com>
2023-08-02 14:40:12 +0600

    set token in header

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,10 +1,27 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { adminLoggedOut } from "../auth/authSlice";
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: process.env.REACT_APP_BASE_URL,
+  prepareHeaders: async (headers, { getState, endpoint }) => {
+    const token = getState()?.adminAuth?.accessToken;
+    if (token) {
+      headers.set("Authorization", `Bearer ${token}`);
+    }
+    return headers;
+  },
+});
 
 export const apiSlice = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({
-    baseUrl: process.env.REACT_APP_BASE_URL,
-  }),
+  baseQuery: async (args, api, extraOptions) => {
+    let result = await baseQuery(args, api, extraOptions);
+    if (result?.error?.status === 500) {
+      api.dispatch(adminLoggedOut());
+      localStorage.clear();
+    }
+    return result;
+  },
   tagTypes: [],
   endpoints: (builder) => ({}),
 });
