059eda5ec17f57002753cef77d1896d794d51fc7
sksabbirhossain <sksabbirhossain4@gmail.com>
2023-08-20 00:54:41 +0600

    created userAuthApi

---
+++ src/features/auth/userAuthApi.js
@@ -0,0 +1,41 @@
+import { apiSlice } from "../api/apiSlice";
+import { userLoggedIn } from "./userAuthSlice";
+
+export const userAuthApi = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    userRegister: builder.mutation({
+      query: (data) => ({
+        url: "/register",
+        method: "POST",
+        body: data,
+      }),
+    }),
+    userLogin: builder.mutation({
+      query: (data) => ({
+        url: "/login",
+        method: "POST",
+        body: data,
+      }),
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          const result = await queryFulfilled;
+          localStorage.setItem(
+            "userAuth",
+            JSON.stringify({
+              accessToken: result.data.data.accessToken,
+              user: result.data.data.user,
+            })
+          );
+          dispatch(
+            userLoggedIn({
+              accessToken: result.data.data.accessToken,
+              user: result.data.data.user,
+            })
+          );
+        } catch (err) {}
+      },
+    }),
+  }),
+});
+
+export const { useUserRegisterMutation, useUserLoginMutation } = userAuthApi;
